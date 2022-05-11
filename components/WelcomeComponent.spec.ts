698fff6cd4dfe757eb37c51d155cf09f1250f5ba
weicheng <weicheng2138@gmail.com>
2022-05-11 22:17:04 +0800

    test: implement vitest for ssr testing #3

---
+++ components/WelcomeComponent.spec.ts
@@ -0,0 +1,20 @@
+import { fileURLToPath } from 'node:url'
+import { describe, expect, it } from 'vitest'
+import { setup, $fetch, isDev } from '@nuxt/test-utils'
+
+describe('SSR Index Page', async () => {
+  await setup({
+    rootDir: fileURLToPath(new URL('..', import.meta.url)),
+    server: true,
+  })
+
+  it('Renders Title of  Get Started', async () => {
+    expect(await $fetch('/')).toMatch('Get Started')
+  })
+
+  if (isDev()) {
+    it('[dev] ensure vite client script is added', async () => {
+      expect(await $fetch('/')).toMatch('/_nuxt/@vite/client"')
+    })
+  }
+})
