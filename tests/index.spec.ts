c705d56150ea3b72eeefde087aec7d0923fdf510
weicheng <weicheng2138@gmail.com>
2022-05-12 15:57:45 +0800

    test: refactor folder for test and e2e test #3

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -7,14 +7,15 @@ describe('SSR Index Page', async () => {
     rootDir: fileURLToPath(new URL('..', import.meta.url)),
     server: true,
   })
-
   it('Renders Title of  Get Started', async () => {
     expect(await $fetch('/')).toMatch('Get Started')
   })
-
   if (isDev()) {
     it('[dev] ensure vite client script is added', async () => {
       expect(await $fetch('/')).toMatch('/_nuxt/@vite/client"')
     })
   }
+  it('go go go', () => {
+    expect(1).toBe(1)
+  })
 })
